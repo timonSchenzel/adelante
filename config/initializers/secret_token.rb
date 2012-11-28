f6e1da8fe9fcb9889477a71519f9d0eb41fb1930
Timon Schenzel <timon_schenzel@msn.com>
2012-11-28 11:48:24 +0100

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Adelante::Application.config.secret_token = 'a516a14ef45eb87451179304005dd908a757b5c787d6fdf0a72b8bfed4af6aacf7d636642c5ea3250b539b455b11ca757b0402b382b58de9d64b1dccfeafcab6'
