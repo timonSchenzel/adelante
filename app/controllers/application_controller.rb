88db81d2811a6ca2ae64fec5c4ee7f1d651baf22
Timon Schenzel <timon_schenzel@msn.com>
2012-12-13 13:14:53 +0100

    - Bootstrap error from heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   include SessionHelper
 
-  @render_header = true
-  @render_footer = true
+  #@render_header = true
+  #@render_footer = true
 end
