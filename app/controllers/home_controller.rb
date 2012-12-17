2533c19ddde9e2549a7c9bceb5fdf90ffca908c3
Dirk Hoffman <dirk.hoffman@live.nl>
2012-12-17 11:49:21 +0100

    Models

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
   #before_filter :signed_in_user
 
   def index
-    @render_header = false
+    @render_header = false;
     @render_footer = false
   end
 
