80c5f3be0b8d2bc10417be36c3f3bd5348d2970c
Timon Schenzel <timon_schenzel@msn.com>
2012-12-17 12:20:46 +0100

    Added therapy controller

---
+++ app/controllers/therapy_controller.rb
@@ -0,0 +1,14 @@
+class TherapyController < ApplicationController
+
+  before_filter :signed_in_user
+
+  def index
+    @therapy = Therapy.new
+    #@game.rounds.build
+  end
+
+  def home
+    @therapy = Therapy.new
+  end
+
+end
