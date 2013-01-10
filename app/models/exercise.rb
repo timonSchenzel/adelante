f71acfa78cea2102ea0c307fd17cf57d28080b4c
Dirk Hoffman <dirk.hoffman@live.nl>
2013-01-10 09:58:47 +0100

    Exercise Has many therapies

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,12 +1,16 @@
 class Exercise < ActiveRecord::Base
   attr_accessible :description, :name, :files_attributes
 
-  belongs_to :therapies_exercises
+  has_many :therapies_exercises
+  has_many :therapies, :through => :therapies_exercises
+
   has_many :ratings
   has_many :files_exercises
   has_many :files, :through => :files_exercises
 
   has_many :clientsessions_exercises
   has_many :clientsessions, :through => :clientsessions_exercises
+
+
   accepts_nested_attributes_for :files
 end
