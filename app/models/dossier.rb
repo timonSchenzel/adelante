094b593143ec1a2abc9bcfcdd71614d916ac6c44
Timon Schenzel <timon_schenzel@msn.com>
2013-01-15 13:57:53 +0100

    - Heroku errors fixed

--- app/models/dossier.rb
+++ app/models/dossier.rb
@@ -1,5 +1,5 @@
 class Dossier < ActiveRecord::Base
   attr_accessible :client_id, :name
 
-  has_many dossier_items
+  has_many :dossier_items
 end
--- app/models/dossier_item.rb
+++ app/models/dossier_item.rb
@@ -1,4 +1,4 @@
 class DossierItem < ActiveRecord::Base
   attr_accessible :description, :dossier_id, :name
-  belongs_to dossier
+  belongs_to :dossier
 end
