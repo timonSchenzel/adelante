c17b66a8b3dc6ca343418b5c3248b0f065fb6f13
Timon Schenzel <timon_schenzel@msn.com>
2013-01-15 14:48:46 +0100

    - Change default url to heroku url

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,10 +3,12 @@ class HomeController < ApplicationController
   before_filter :signed_in_user
 
   def index
+    @all_therapies = Therapy.all
     @therapy = Therapy.new
   end
 
   def home
+    @all_therapies = Therapy.all
     @therapy = Therapy.new
     1.times do |number|
       @therapy.exercises.build
